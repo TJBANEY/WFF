a2fb8aa8a4ae7db7b2f35ce6f5a1030daad77e50
Timothy Baney <timbaney1989@gmail.com>
2017-05-20 16:27:56 -0500

    Remove redundant imports.

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,9 +4,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 
-from navigation import views as nav_views
-from navigation.navSiteMap import NavSitemap
-from page_content.page_site_map import PageSiteMap
 from webapp.views import StaticViewSitemap
 from . import views as webapp_views
 
@@ -14,18 +11,11 @@ admin.site.site_header = 'Site Header'
 admin.site.index_title = 'Site Index Title'
 admin.site.site_title = 'Site Title'
 
-site_maps = {
-    'static': StaticViewSitemap,
-    'navigation': NavSitemap,
-    'pages': PageSiteMap,
-}
-
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^$', webapp_views.view_home, name='home'),
     url(r'^inplaceeditform/', include('inplaceeditform.urls')),
-    url(r'^sitemap\.xml$', sitemap, {'sitemaps': site_maps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^admin/filebrowser/', include('filebrowser.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
@@ -34,4 +24,4 @@ if settings.DEVELOPMENT:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # This url pattern must be last since it will match on anything
-urlpatterns += [url(r'^.+/', nav_views.default), ]
+# urlpatterns += [url(r'^.+/', nav_views.default), ]
