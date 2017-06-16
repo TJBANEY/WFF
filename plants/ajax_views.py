93adc5e5e1e64af774d3a1e8adc8463cf7ffe3e8
Timothy Baney <timbaney1989@gmail.com>
2017-06-16 00:39:53 -0500

    Create ajax function for registration that will retrieve plants searched for.

--- plants/ajax_views.py
+++ plants/ajax_views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 
 from plants.models import Plant
@@ -8,10 +9,15 @@ from plants.models import Plant
 
 @csrf_exempt
 def get_plants(request):
-	all_plants = Plant.objects.all()
+	body_unicode = request.body.decode('utf-8')
+	value = body_unicode.split('=')[1]
+
+	plants = Plant.objects.filter(botanical_name__icontains=value)
+
+	results = render_to_string('plants/ajax/plant_results.html', {'plants': plants})
 
 	json_data = {
-
+		'results': results
 	}
 
 	return HttpResponse(json.dumps(json_data), content_type='application/json')
