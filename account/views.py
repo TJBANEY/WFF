1720607bff197269e3509689f7d642fae0ececef
Levi Donaldson <lcdbacanas@gmail.com>
2018-02-07 21:56:23 -0600

    added new page

--- account/views.py
+++ account/views.py
@@ -105,7 +105,7 @@ def sign_in(request):
 		if form.is_valid():
 			login(request, form.get_user())
 
-			return HttpResponseRedirect('/calendar')
+			return HttpResponseRedirect('/calendar.html')
 
 	else:
 		form = LoginForm()
--- plant_calendar/views.py
+++ plant_calendar/views.py
@@ -57,6 +57,54 @@ def calendar_home(request, plant):
 	return render(request, 'plant_calendar/calendar.html', context)
 
 
+
+# @login_required(login_url='/account/sign-in')
+# def info(request, plant):
+# 	account = Account.current_user(request)
+# 	if plant:
+# 		plant = plant.rstrip('/')
+
+# 	# Get all plant events for the plants belonging to the user
+# 	account_plants = UserPlant.objects.filter(user=account)
+# 	acct_plant_flatlist = account_plants.values_list('plant', flat=True)
+
+# 	if plant:
+# 		try:
+# 			plant = Plant.objects.get(slug=plant)
+
+# 			if plant.id in acct_plant_flatlist:
+# 				target_plant = plant
+# 			else:
+# 				raise Http404
+
+# 		except Plant.DoesNotExist:
+# 			raise Http404
+# 	else:
+# 		if account_plants:
+# 			target_plant = account_plants[0].plant
+# 		else:
+# 			target_plant = None
+
+# 	all_events = PlantEvent.objects.filter(plant=target_plant)
+
+# 	json_events = json.dumps([{
+# 								  'title': event.name,
+# 								  'start': datetime.datetime.strftime(event.event_start, '%Y-%m-%d'),
+# 								  'end': datetime.datetime.strftime(event.event_end, '%Y-%m-%d')
+# 							  } for event in all_events])
+
+# 	# Format the QuerySet into json that FullCalendar.js can recognize
+
+# 	# fmt_date = all_events.event_start.strftime("%-m/%d/%y")
+
+# 	context = {
+# 		'events': json_events,
+# 		'account_plants': account_plants
+# 	}
+
+# 	return render(request, 'plant_calendar/info.html', context)
+
+
 def angular_views(request, page_name):
 	try:
 		return render(request, 'angular/%s' % page_name)
